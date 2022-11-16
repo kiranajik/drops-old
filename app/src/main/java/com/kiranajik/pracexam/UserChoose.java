bd2450735e6e81c5e87ca975bb4c4761f2e99a4b
midhun <midhunmurali776@gmail.com>
2022-11-16 20:42:42 +0530

    New Change

---
+++ app/src/main/java/com/kiranajik/pracexam/UserChoose.java
@@ -0,0 +1,14 @@
+package com.kiranajik.pracexam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserChoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_choose);
+    }
+}
